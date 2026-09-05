3bbdae187b2bc9b269aab9e1f54a2f3ac59359a7
Maheshwari Peddapirangi <maheshwaripeddapirang@gmail.com>
2026-09-05 18:12:59 +0530

    allowed host changed

--- hash_project/settings.py
+++ hash_project/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["sep5-2.onrender.com",
+ALLOWED_HOSTS = ["sep5-3.onrender.com",
     "localhost",
-    "127.0.0.1",]
+    "127.0.0.1"]
 
 
 # Application definition
