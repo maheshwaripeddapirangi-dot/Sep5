62414a3246da563bd2f87d6dd55134dcf0462a96
Maheshwari Peddapirangi <maheshwaripeddapirang@gmail.com>
2026-09-05 17:30:58 +0530

    Changed

--- hash_project/settings.py
+++ hash_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sep5.onrender.com','localhost:8000']
 
 
 # Application definition
